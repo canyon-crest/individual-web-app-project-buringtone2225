4c9deb3979aad2e42e3e0aad5f32cefae901f9d0
buringtone2225 <buringtone2225@my.sduhsd.net>
2026-03-04 05:12:10 +0000

    Finished css and added interactive elements w/ js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // add javascript here
+document.getElementById("toggle-species")?.addEventListener("click", () => {
+    const species = document.getElementById("daily-species");
+    species.classList.toggle("hidden");
+})
+
+document.getElementById("speciesForm")?.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const speciesInput = document.getElementById("species");
+    const list = document.getElementById("list");
+
+    if (speciesInput.value.trim() !== "") {
+        const newItem = document.createElement("li");
+        newItem.textContent = speciesInput.value;
+        list.appendChild(newItem);
+        speciesInput.value = "";
+    }
+});
